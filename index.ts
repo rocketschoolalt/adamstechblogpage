fa72d04f8f697aa159311436ad9e38bf6d3fc663
rocketschoolalt <153968494+rocketschoolalt@users.noreply.github.com>
2024-01-27 21:25:15 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -53,9 +53,13 @@ seoManager.setMetaTag('description', 'Stay ahead of the curve with cutting-edge 
 // Add more tags as needed
 
 seoManager.setMetaTag('keywords', 'technology news, tech trends, latest gadgets, tech innovations, tech blog');
-seoManager.setMetaTag('canonical', 'https://www.adamstechblog.deno.dev');
+seoManager.setMetaTag('canonical', 'https://adamstechblog.deno.dev');
 seoManager.setMetaTag('og:title', 'Latest Tech News & Advancements - The Advancements of Technology: Adams Tech Blog');
 seoManager.setMetaTag('og:description', 'Discover the latest trends and innovations in technology on our blog.');
-seoManager.setMetaTag('og:image', 'https://www.adamstechblog.deno.dev/');
-seoManager.setMetaTag('og:url', 'https://www.yourtechblog.com');
+seoManager.setMetaTag('og:image', 'https://adamstechblog.deno.dev/posts/pfp.jpg');
+seoManager.setMetaTag('og:url', 'https://adamstechblog.deno.dev');
 seoManager.setMetaTag('og:type', 'website');
+seoManager.setMetaTag('twitter:card', 'summary_large_image');
+seoManager.setMetaTag('twitter:title', 'Latest Tech News & Advancements - The Advancements of Technology: Adams Tech Blog');
+seoManager.setMetaTag('twitter:description', 'Explore the forefront of technology with our in-depth analysis and news.');
+seoManager.setMetaTag('author', 'Adam Ahmed');
